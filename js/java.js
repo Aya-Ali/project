d2347c69ec0a94d46a7fd4299113bbeca04c6e10
Aya-Ali <ayaali13477@gmail.com>
2018-12-30 23:00:40 +0200

    firstCommit

---
+++ js/java.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  
+    $("#features").animate({transform: "scale(1.2)"},500)
+
+})
+
+
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+/************************************************************/
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+new WOW().init();
