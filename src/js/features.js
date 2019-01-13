7a0a3e79cc04d40e8ba8f1204cbe615b66583eb8
ayaleap <aya.ali@leap13.com>
2019-01-14 00:48:18 +0200

    "Webpack-Task"

--- src/js/features.js
+++ src/js/features.js
@@ -2,3 +2,4 @@
 /*@import 'blog.css';
 @import 'featuers.css';
 */
+import css from '../css/features.css';
--- src/js/template.js
+++ src/js/template.js
@@ -1,4 +1,4 @@
-
+if($('.chang').length>0){
 var typed2 = new Typed('.chang', {
     strings: ['To sell my potter','To promote my restaurant', 'A blog about my travels', 'A photography portfolio'],
     typeSpeed: 100,
@@ -15,14 +15,12 @@ $(document).ready(function(){
     columnWidth: '.grid-item'
   },
 });
-    $('.show2').on( 'click', 'a', function() {
+$('.show2').on( 'click', 'a', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
         $(".show2 a").removeClass("active");
         $(this).addClass("active");
 });
 });
-
-
-
-
+}
+import css from '../css/template.css';
